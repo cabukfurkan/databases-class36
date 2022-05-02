d8a9b571326bf88004d48b2d902ebea2213633f6
Furkan Cabuk <furkancabuk.ce@gmail.com>
2022-05-03 00:19:47 +0200

    first Commit: Exercise 1 and 2 done

---
+++ Week1/exercise1.js
@@ -0,0 +1,85 @@
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "meetup",
+});
+
+const queryDropDatabase = "DROP DATABASE IF EXISTS meetup;";
+
+const queryCreateDatabase = "CREATE DATABASE meetup;";
+
+const queryUseDatabase = "USE meetup;";
+
+const queryCreateInviteTable = `CREATE TABLE Invitee(
+  invitee_no SMALLINT, 
+  invitee_name VARCHAR(30), 
+  invited_by VARCHAR(30)
+);`;
+
+const queryCreateMeetingTable = `CREATE TABLE Meeting(
+  meeting_no SMALLINT, 
+  meeting_title VARCHAR(100), 
+  starting_time DATETIME, 
+  ending_time DATETIME, 
+  room_no SMALLINT
+);`;
+
+const queryCreateRoomTable = `CREATE TABLE Room(
+  room_no SMALLINT, 
+  room_name VARCHAR(30), 
+  floor_number SMALLINT
+);`;
+
+const queryInsertIntoInvitee = `INSERT INTO Invitee (
+  invitee_no, invitee_name, invited_by) 
+VALUES 
+  (1, 'Edward', 'Rob'),
+  (2, 'Mohammed', 'Rob'),
+  (3, 'Ali', 'Rob'),
+  (4, 'Suleyman', 'Rob'),
+  (5, 'Radhi', 'Rob');`;
+
+const queryInsertIntoMeeting = `INSERT INTO Meeting (
+  meeting_no, meeting_title, starting_time, ending_time, room_no)
+VALUES 
+  (1, 'HTML CSS GIT', '2022-05-02 10:00:00', '2022-05-02 11:00:00', 701),
+  (2, 'JAVASCRIPT', '2022-05-02 10:00:00', '2022-05-02 11:00:00', 702),
+  (3, 'BROWSERS', '2022-05-02 11:00:00', '2022-05-02 12:00:00', 703),
+  (4, 'NODEJS', '2022-05-02 11:00:00', '2022-05-02 12:00:00', 704),
+  (5, 'DATABASES', '2022-05-02 12:00:00', '2022-05-02 13:00:00', 705);`;
+
+const queryInsertIntoRoom = `INSERT INTO Room (
+  room_no, room_name, floor_number) 
+  VALUES 
+  (701, 'Alkmaar', 7),
+  (702, 'Den Helder', 7),
+  (703, 'Utrecht', 7),
+  (704, 'Rotterdam', 7),
+  (705, 'Haarlem', 7);`;
+
+const execQuery = (query) => {
+  connection.query(query, (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log(results);
+    }
+  });
+};
+
+connection.connect();
+
+execQuery(queryDropDatabase);
+execQuery(queryCreateDatabase);
+execQuery(queryUseDatabase);
+execQuery(queryCreateInviteTable);
+execQuery(queryCreateMeetingTable);
+execQuery(queryCreateRoomTable);
+execQuery(queryInsertIntoInvitee);
+execQuery(queryInsertIntoMeeting);
+execQuery(queryInsertIntoRoom);
+
+connection.end();
---
+++ Week1/exercise2.js
@@ -0,0 +1,45 @@
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "hyfuser",
+    password: "hyfpassword",
+    database: "new_world",
+});
+
+connection.connect();
+
+const execQuery = (query) => {
+    connection.query(query, (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            console.table(results);
+        }
+    });
+};
+
+const queryCountriesGreater8m = "SELECT Name FROM country WHERE Population > 8000000;"
+const queryCountriesWLandInIt = "SELECT Name FROM country WHERE Name LIKE '%land%';"
+const queryCitiesBw5t1m = "SELECT Name FROM city WHERE Population BETWEEN 500000 AND 1000000;"
+const queryCountriesInEu = "SELECT Name FROM country WHERE Continent = 'Europe';"
+const queryCountriesOrderDesc = "SELECT Name FROM country ORDER BY SurfaceArea DESC;"
+const queryCitiesInNl = "SELECT Name FROM city WHERE CountryCode = 'NLD';"
+const queryPopulationRotterdam = "SELECT Population FROM city WHERE Name = 'Rotterdam';"
+const queryCountriesBigTop10 = "SELECT * FROM country ORDER BY SurfaceArea DESC LIMIT 10;"
+const queryCitiesCrowdedTop10 = "SELECT * FROM city ORDER BY Population DESC LIMIT 10;"
+const queryPopulationWorld = "SELECT SUM(Population) FROM country;"
+
+
+execQuery(queryCountriesGreater8m);
+execQuery(queryCountriesWLandInIt);
+execQuery(queryCitiesBw5t1m);
+execQuery(queryCountriesInEu);
+execQuery(queryCountriesOrderDesc);
+execQuery(queryCitiesInNl);
+execQuery(queryPopulationRotterdam);
+execQuery(queryCountriesBigTop10);
+execQuery(queryCitiesCrowdedTop10);
+execQuery(queryPopulationWorld);
+
+connection.end();
