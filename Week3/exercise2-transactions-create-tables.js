d2659cb443c9fef05a0bfa218d1e45382f87d605
Furkan Cabuk <furkancabuk.ce@gmail.com>
2022-05-22 23:39:24 +0200

    data type of remark is changed to TEXT

--- Week3/exercise2-transactions-create-tables.js
+++ Week3/exercise2-transactions-create-tables.js
@@ -2,54 +2,54 @@ const util = require('util');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'hyfuser',
-    password: 'hyfpassword',
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
 });
 
 const execQuery = util.promisify(connection.query.bind(connection));
 
 async function seedDatabase() {
-    const queryDatabaseDrop = "DROP DATABASE IF EXISTS userdb;";
-    const queryDatabaseCreate = "CREATE DATABASE userdb;";
-    const queryDatabaseUse = "USE userdb;";
+  const queryDatabaseDrop = "DROP DATABASE IF EXISTS userdb;";
+  const queryDatabaseCreate = "CREATE DATABASE userdb;";
+  const queryDatabaseUse = "USE userdb;";
 
-    const createTableAccount = `
+  const createTableAccount = `
   CREATE TABLE account (
     accountNumber INT,
     balance BIGINT,
     PRIMARY KEY (accountNumber)
   );`;
 
-    const createTableAccountChanges = `
+  const createTableAccountChanges = `
   CREATE TABLE accountChanges (
     changeNumber INT,
     accountNumber INT,
     amount BIGINT,
     changedDate DATETIME,
-    remark VARCHAR(600),
+    remark TEXT,
     FOREIGN KEY (accountNumber) REFERENCES account(accountNumber),
     PRIMARY KEY (changeNumber, accountNumber)
   );`;
 
-    connection.connect();
+  connection.connect();
 
-    try {
-        await execQuery('START TRANSACTION');
-        await execQuery(queryDatabaseDrop);
-        await execQuery(queryDatabaseCreate);
-        await execQuery(queryDatabaseUse);
-        await execQuery(createTableAccount);
-        await execQuery(createTableAccountChanges);
+  try {
+    await execQuery('START TRANSACTION');
+    await execQuery(queryDatabaseDrop);
+    await execQuery(queryDatabaseCreate);
+    await execQuery(queryDatabaseUse);
+    await execQuery(createTableAccount);
+    await execQuery(createTableAccountChanges);
 
-        await execQuery('COMMIT');
-    } catch (error) {
-        console.error(error);
-        await execQuery('ROLLBACK');
-        connection.end();
-    }
-
+    await execQuery('COMMIT');
+  } catch (error) {
+    console.error(error);
+    await execQuery('ROLLBACK');
     connection.end();
+  }
+
+  connection.end();
 }
 
 seedDatabase();
